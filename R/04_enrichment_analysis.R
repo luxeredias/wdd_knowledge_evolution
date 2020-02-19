f1549021ff67e7e038445d418cdf804750967d64
Thomaz Luscher Dias <luxeredias@gmail.com>
2020-02-19 15:00:25 -0300

    updates to enrichment evolution

--- R/04_enrichment_analysis.R
+++ R/04_enrichment_analysis.R
@@ -43,13 +43,15 @@ data("diseases")
 #useful objects
 classes <- c("inflammatory","psychiatric","infectious")
 color <- c("#ff743e","#00b7da","#68ad36")
+inflammatory_diseases <- unique(edges_list[[1]]$Target)
+psychiatric_diseases <- unique(edges_list[[2]]$Target)
+infectious_diseases <- unique(edges_list[[3]]$Target)
 
 #edit Reactome GMT file
 reactome_GMT_converted$reactome_Id <- as.character(str_extract_all(string = reactome_GMT_converted$ont,
                                                       pattern = "R-HSA-\\d+"))
 reactome_GMT_converted$ont <- str_remove_all(string = reactome_GMT_converted$ont,
                                              pattern = " Homo sapiens R-HSA-\\d+")
-reactome_GMT_converted <- read.gmt("data/ReactomePathwaysLevel3.gmt")
 
 picked_terms <- read.csv("data/picked_terms_reactome.txt",col.names = "term")
 reactome_GMT_selected <- reactome_GMT_converted %>%
@@ -67,10 +69,10 @@ all_genes <- all_edges_converted %>%
   unique()
 
 #select the top 9 most connected diseases in 2018
-top_9_all <- c()
+top_9_list <- list()
 Reactome_results_all_list <- list()
 selected_terms_list <- list()
-selected_terms_years_list <- list()
+all_terms_list <- list()
 for (i in 1:3){
   #selecte edges of each disease type
   class <- classes[i]
@@ -79,11 +81,12 @@ for (i in 1:3){
     dplyr::filter(Target %in% diss)
   degrees <- table(edge$Target)
   top_9 <- as.character(as.data.frame(degrees[order(degrees,decreasing = T)])[1:9,1])
-  top_9_all <- c(top_9_all,top_9)
+  top_9_list[[i]] <- top_9
   
   #perform functional enrichment against Reactome for each of the top9 disease's genes
-  for (j in 1:length(top_9)){
-    dis <- top_9[j]
+  for (j in 1:length(diss)){
+    dis <- diss[j]
+    #dis <- top_9[j]
     genes <- edge %>%
       dplyr::filter(Target == dis) %>%
       dplyr::pull(Source) %>%
@@ -115,29 +118,38 @@ for (i in 1:3){
   
   #save aggregated results to Reactome_results_all list
   Reactome_results_all_list[[i]] <- reactome_results_all
+  all_terms <- unique(reactome_results_all$Term)
+  all_terms_list[[i]] <- all_terms
+  enrichment_filename <- paste0("data/all_",class,"_reactome_enrichment.csv")
+  write.csv(reactome_results_all,file = enrichment_filename,quote = F,row.names = F)
   
-  #plot results
+  # #plot results
+  # data_plot <- reactome_results_all
+  # data_plot[is.na(data_plot)] <- 1
+  # data_plot <- data_plot %>%
+  #   column_to_rownames("Term")
+  # data_plot <- -log(data_plot)
+  # filename <- paste0("figures/",classes[i],"_Reactome.svg")
+  # svg(filename,width = length(diss),height = length(rownames(data_plot)))
+  # heatmap(as.matrix(data_plot),
+  #         col=pal,scale = "none",margins = c(10,30), #margin = c(col,row)
+  #         cexRow = .6,
+  #         cexCol = .5)
+  # dev.off()
+  
+  #get the 20 terms that are enriched in the highest number of diseases
+  #transform enrichment results in -log10pvalue
   data_plot <- reactome_results_all
   data_plot[is.na(data_plot)] <- 1
   data_plot <- data_plot %>%
     column_to_rownames("Term")
   data_plot <- -log(data_plot)
-  pal <- colorRampPalette(c("white",color[i]))(1000)
-  filename <- paste0("figures/",classes[i],"_Reactome.svg")
-  svg(filename,width = 10,height = 10)
-  heatmap(as.matrix(data_plot),
-          col=pal,scale = "none",margins = c(10,30), #margin = c(col,row)
-          cexRow = .6,
-          cexCol = .5)
-  dev.off()
   
-  #select terms to be used in evolution analysis: terms that were enriched
-  #in the majority of the top 9 diseases in each category
-  
-  #get the 20 terms that are enriched in the highest number of diseases
+  #enriched terms dataframe
   terms <- data.frame(term=unique(rownames(data_plot)),
                       enriched_in=as.numeric(""))
   
+  #calculate the number of diseases with enrichment for each term
   for (t in 1:length(terms$term)){
     term <- terms$term[t]
     df <- data_plot %>%
@@ -147,6 +159,7 @@ for (i in 1:3){
     terms$enriched_in[t] <- diseases_with_enrichment
   }
   
+  #select the top 20 terms enriched in more diseases
   selected_terms <- terms %>%
     arrange(desc(enriched_in)) %>%
     pull(term) %>%
@@ -154,8 +167,12 @@ for (i in 1:3){
   
   selected_terms_list[[i]] <- selected_terms
   
-  data_selected <- data_plot[rownames(data_plot) %in% selected_terms,]
+  #subset enrichment table with top 20 terms and top 9 diseases
+  data_selected <- data_plot[rownames(data_plot) %in% selected_terms,top_9]
+  
+  #plot
   filename <- paste0("figures/",classes[i],"_selected_terms.svg")
+  pal <- colorRampPalette(c("white",color[i]))(1000)
   svg(filename,width = 5,height = 5)
   heatmap(as.matrix(data_selected[,colSums(data_selected)>0]),
           col=pal,scale = "none",margins = c(8,15), #margin = c(col,row)
@@ -166,8 +183,10 @@ for (i in 1:3){
 }
 
 all_selected_terms <- unique(unlist(selected_terms_list))
+all_terms <- unique(unlist(all_terms_list))
 
-common_terms <- Reduce(intersect,selected_terms_list)
+common_selected_terms <- Reduce(intersect,selected_terms_list)
+common_all_terms <- Reduce(intersect,all_terms_list)
 
 inflammatory_unique_terms <- setdiff(selected_terms_list[[1]],common_terms)
 psichiatric_unique_terms <- setdiff(selected_terms_list[[2]],common_terms)
@@ -175,13 +194,22 @@ infectious_unique_terms <- setdiff(selected_terms_list[[3]],common_terms)
 
 
 #perform enrichment for all diseases in each year for the selected terms
+evolution_reactome_list <- list()
+evolution_reactome_melt_list <- list()
 for (i in 1:27){
-  dis <- top_9_all[i]
+  dis <- unlist(top_9_list)[i]
   df <- all_edges_converted %>%
     filter(Target==dis)
   cols <- colnames(df)[-c(1,2)]
+  #perform enrichment for disease in each year
   for (j in 1:29){
     col <- which(colnames(df)==cols[j])
+    genes_year <- all_edges_converted %>%
+      select(1,2,col) %>%
+      rename(docs=3) %>%
+      filter(docs > 0) %>%
+      pull(Source) %>%
+      unique()
     
     genes <- df %>%
       select(1,2,31) %>%
@@ -189,24 +217,14 @@ for (i in 1:27){
       filter(docs > 0) %>%
       pull(Source) %>%
       unique()
+    
     #reactome enrichment
     if (length(genes)>0){
-      tryCatch({
         reactome_results <- enricher(gene = genes,pvalueCutoff = 0.05,
                                      pAdjustMethod = "BH",
-                                     universe = all_genes,
+                                     universe = genes_year,
                                      TERM2GENE = reactome_GMT_converted[,-3])
-        reactome_results <- reactome_results@result
-        reactome_results <- reactome_results %>%
-          filter(pvalue < 0.05,Count>0) %>%
-          dplyr::select(ID,p.adjust)
-        colnames(reactome_results) <- c("Term",dis)
-        if (j == 1){
-          reactome_results_all <- reactome_results
-        }else{
-          reactome_results_all <- reactome_results_all %>% 
-            full_join(reactome_results,by="Term")
-        }},error=function(err){
+        if (length(reactome_results)==0){
           reactome_results <- data.frame(Term="",x=as.numeric(""))
           colnames(reactome_results) <- c("Term",dis)
           if (j == 1){
@@ -215,8 +233,21 @@ for (i in 1:27){
             reactome_results_all <- reactome_results_all %>% 
               full_join(reactome_results,by="Term")
           }
-        })
-    }else{
+        }else{
+          reactome_results <- reactome_results@result
+          reactome_results <- reactome_results %>%
+            filter(pvalue < 0.05,Count>0) %>%
+            dplyr::select(ID,p.adjust)
+          colnames(reactome_results) <- c("Term",dis)
+          if (j == 1){
+            reactome_results_all <- reactome_results
+          }else{
+            reactome_results_all <- reactome_results_all %>% 
+              full_join(reactome_results,by="Term")
+          }
+        }
+      
+      }else{
       reactome_results <- data.frame(Term="",x=as.numeric(""))
       colnames(reactome_results) <- c("Term",dis)
       if (j == 1){
@@ -226,17 +257,43 @@ for (i in 1:27){
           full_join(reactome_results,by="Term")
       }
     }
-  }
   }#end of enrichment loop for each disease in the given year
-  rownames(reactome_results_all) <- reactome_results_all$Term
-  reactome_results_all$Term <- NULL
-  reactome_results_all[is.na(reactome_results_all)] <- 1
-  reactome_results_all <- -log(reactome_results_all)
   
-  evolution_reactome_list[[k]] <- reactome_results_all
+  reactome_results_all <- reactome_results_all[-1,]
+  reactome_results_all[,-1][is.na(reactome_results_all[,-1])] <- 1
+  reactome_results_all[,-1] <- -log(reactome_results_all[,-1])
+  colnames(reactome_results_all) <- c("Term",seq(1990,2018,1))
+  evolution_reactome_list[[i]] <- reactome_results_all
+  
+  reactome_results_melt <- melt(reactome_results_all)
+  reactome_results_melt$dis <- dis
+  evolution_reactome_melt_list[[i]] <- reactome_results_melt
 }
 
+names(evolution_reactome_list) <- unlist(top_9_list)
 
+evolution_reactome_melt <- bind_rows(evolution_reactome_melt_list)
 
+evolution_reactome_melt$class <- ifelse(evolution_reactome_melt$dis %in% inflammatory_diseases,yes = "inflammatory",no="")
+evolution_reactome_melt$class <- ifelse(evolution_reactome_melt$dis %in% psychiatric_diseases,yes = "psychiatric",no=evolution_reactome_melt$class)
+evolution_reactome_melt$class <- ifelse(evolution_reactome_melt$dis %in% infectious_diseases,yes = "infectious",no=evolution_reactome_melt$class)
 
-
+pdf("figures/all_terms_evolution.pdf")
+for (i in 1:length(unique(evolution_reactome_melt$Term))){
+  term <- unique(evolution_reactome_melt$Term)[i]
+  p<-evolution_reactome_melt %>%
+    filter(Term==term) %>%
+    ggplot(aes(x=variable,y=dis,fill=class,alpha=log(value+1)))+
+      geom_tile(show.legend = F)+
+      theme_minimal(base_line_size = .1)+
+      theme(axis.title.y = element_blank(),
+            #axis.text.y = element_blank(),
+            axis.text.x = element_blank())+
+      scale_x_discrete(limits = c(1990,2000,2010,2018),name="Year")+
+      facet_grid(rows = vars(class),cols = vars(Term), scales = "free", space = "free")+
+      #facet_wrap(facets = ~term,scales = "free")+
+      scale_fill_manual(values = c(color[3],color[1],color[2]))+
+      theme(strip.text = element_text(size = 8))
+  print(p)
+}  
+dev.off()
